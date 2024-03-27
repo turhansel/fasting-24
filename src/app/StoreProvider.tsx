a5a71030dfbedf98db07ef7cdef869e6990a8e10
Turhan <turhansel@gmail.com>
2024-03-27 12:00:22 +0300

    feat: configure redux structure

---
+++ src/app/StoreProvider.tsx
@@ -0,0 +1,34 @@
+'use client';
+import { AppStore, makeStore } from '@/lib/redux/store';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import type { ReactNode } from 'react';
+import { useEffect, useRef } from 'react';
+import { Provider } from 'react-redux';
+
+interface Props {
+	readonly children: ReactNode;
+}
+
+export const StoreProvider = ({ children }: Props) => {
+	const storeRef = useRef<AppStore | null>(null);
+
+	if (!storeRef.current) {
+		// Create the store instance the first time this renders
+		storeRef.current = makeStore();
+	}
+
+	useEffect(() => {
+		if (storeRef.current != null) {
+			// configure listeners using the provided defaults
+			// optional, but required for `refetchOnFocus`/`refetchOnReconnect` behaviors
+			const unsubscribe = setupListeners(storeRef.current.dispatch);
+			return unsubscribe;
+		}
+	}, []);
+
+	return (
+		// <ProtectedProvider>
+		<Provider store={storeRef.current}>{children}</Provider>
+		// </ProtectedProvider>
+	);
+};
---
+++ src/lib/redux/store.ts
@@ -0,0 +1,33 @@
+import type { Action, ThunkAction } from '@reduxjs/toolkit';
+import { combineSlices, configureStore } from '@reduxjs/toolkit';
+import { authApiSlice } from './features/auth/authApiSlice';
+
+// `combineSlices` automatically combines the reducers using
+// their `reducerPath`s, therefore we no longer need to call `combineReducers`.
+const rootReducer = combineSlices(authApiSlice);
+// Infer the `RootState` type from the root reducer
+export type RootState = ReturnType<typeof rootReducer>;
+
+// `makeStore` encapsulates the store configuration to allow
+// creating unique store instances, which is particularly important for
+// server-side rendering (SSR) scenarios. In SSR, separate store instances
+// are needed for each request to prevent cross-request state pollution.
+export const makeStore = () => {
+	return configureStore({
+		reducer: rootReducer,
+		middleware: (getDefaultMiddleware) => {
+			return getDefaultMiddleware().concat(authApiSlice.middleware); // could be array
+		},
+	});
+};
+
+// Infer the return type of `makeStore`
+export type AppStore = ReturnType<typeof makeStore>;
+// Infer the `AppDispatch` type from the store itself
+export type AppDispatch = AppStore['dispatch'];
+export type AppThunk<ThunkReturnType = void> = ThunkAction<
+	ThunkReturnType,
+	RootState,
+	unknown,
+	Action
+>;
