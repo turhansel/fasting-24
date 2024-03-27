1d4b16c6c1304dac72ace34df40fe33760e43bca
Turhan <turhansel@gmail.com>
2024-03-27 18:21:40 +0300

    chore: update error pages

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,6 +6,8 @@ import Link from 'next/link';
 export default function NotFound() {
 	return (
 		<div className='min-h-screen flex items-center flex-col justify-center'>
+			<h2 className='text-2xl text-black'>Something went wrong!</h2>
+
 			<Link href={'/'}>
 				<Button
 					className='mt-2 bg-slate-600'
