2128f094abc69741d93dfdb265c251d9b4d2067e
Turhan <turhansel@gmail.com>
2024-03-27 14:22:04 +0300

    feat: create not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
+export default function NotFound() {
+	return (
+		<div className='min-h-screen flex items-center flex-col justify-center'>
+			<Link href={'/'}>
+				<Button
+					className='mt-2 bg-slate-600'
+					onClick={() => window.location.reload()}
+				>
+					Try Again
+				</Button>
+			</Link>
+		</div>
+	);
+}
