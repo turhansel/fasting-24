11c97430f625367b001b791c4f842cf44d592e83
Turhan <turhansel@gmail.com>
2024-03-28 13:22:21 +0300

    refactor: fasting form logic

--- src/components/Fasting/FastingForm/index.tsx
+++ src/components/Fasting/FastingForm/index.tsx
@@ -4,40 +4,18 @@ import { useUseGetDraftFormValuesQuery } from '@/lib/redux/features/fastings/fas
 import CreateForm from './CreateForm';
 import FastingInProgressForm from './FastingInProgressForm';
 import FastingCompletedForm from './FastingPreCompletedForm';
-import { DraftFormResponse } from '@/lib/types';
-
-enum FormType {
-	CREATE = 'CREATE',
-	IN_PROGRESS = 'IN_PROGRESS',
-	PRE_COMPLETED = 'COMPLETED',
-}
-
-const DecideFastingFormType: Record<FormType, React.FC<DraftFormResponse>> = {
-	[FormType.CREATE]: CreateForm,
-	[FormType.IN_PROGRESS]: FastingInProgressForm,
-	[FormType.PRE_COMPLETED]: FastingCompletedForm,
-};
+import React from 'react';
 
 const FastingForm: React.FC = () => {
 	const { data, isLoading } = useUseGetDraftFormValuesQuery();
 
-	let formType = FormType.CREATE;
+	if (isLoading) return <div>Loading...</div>;
 
-	if (isLoading) {
-		return <div>loading...</div>;
-	}
+	if (!!data?.inProgressFasting) return <FastingInProgressForm {...data} />;
 
-	if (data?.inProgressFasting) {
-		formType = FormType.IN_PROGRESS;
-	}
+	if (!!data?.preCompletedFasting) return <FastingCompletedForm {...data} />;
 
-	if (data?.preCompletedFasting) {
-		formType = FormType.PRE_COMPLETED;
-	}
-
-	const DecidedForm = DecideFastingFormType[formType];
-
-	return <DecidedForm {...data} />;
+	return <CreateForm />;
 };
 
 export default FastingForm;
