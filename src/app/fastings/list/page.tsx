a51eaf046b55e9b3d162cdf8a96d7f833e9a2063
Turhan <turhansel@gmail.com>
2024-03-28 04:53:17 +0300

    feat: create listing page

---
+++ src/app/fastings/list/page.tsx
@@ -0,0 +1,12 @@
+import FastingStatistics from '@/components/FastingStatistics';
+import FastingList from '@/components/FastingList';
+
+export default async function Fastings() {
+	return (
+		<>
+			<FastingStatistics />
+
+			<FastingList />
+		</>
+	);
+}
